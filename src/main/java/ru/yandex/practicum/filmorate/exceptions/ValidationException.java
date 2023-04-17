66cb3fde162d936e545c0a16c9df16116ccd61d5
v01cecrack <117996556+v01cecrack@users.noreply.github.com>
2023-04-18 01:58:22 +0300

    Update ValidationException.java

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class ValidationException extends RuntimeException{
-    public ValidationException(String message){
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
         super(message);
     }
 }
