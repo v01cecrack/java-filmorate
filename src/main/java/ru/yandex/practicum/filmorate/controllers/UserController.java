6b544fb51da8146a7f1ab2ca6d041cf48cb53a77
v01cecrack <117996556+v01cecrack@users.noreply.github.com>
2023-04-18 01:56:09 +0300

    Update UserController.java

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -2,14 +2,11 @@ package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.impl.ImplUserService;
 
-import javax.validation.Valid;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -17,20 +14,23 @@ import java.util.Map;
 @Slf4j
 public class UserController {
     private ImplUserService userService;
+
     @PostMapping
-    public User addUser(@RequestBody User user){
+    public User addUser(@RequestBody User user) {
         userService.addUser(user);
         log.info("Добавлен пользователь: {}", user);
         return user;
     }
+
     @PutMapping
-    public User updateUser(@RequestBody User user){
+    public User updateUser(@RequestBody User user) {
         userService.updateUser(user);
         log.info("Обновление данных пользователя: {}", user);
         return user;
     }
+
     @GetMapping
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return userService.getUsers();
     }
 }
